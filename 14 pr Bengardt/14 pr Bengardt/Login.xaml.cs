d7df5fdab290134a03b2b0486738e8b42e10d81d
Ketnez <90082409+Ketnez@users.noreply.github.com>
2021-12-18 12:33:00 +0300

    Add files via upload

---
+++ 14 pr Bengardt/14 pr Bengardt/Login.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _14_pr_Bengardt
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void enter_Click(object sender, RoutedEventArgs e)
+        {
+            if (Password.Password == "123")
+            {
+                Close();
+
+            }
+            else
+            {
+                Password.BorderBrush = Brushes.Red;
+            }
+        }
+
+        private void input_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key < Key.D0 || e.Key > Key.D9)
+            {
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+                    e.Handled = true;
+            }
+            else e.Handled = false;
+        }
+
+        private void input_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
